f3e911c9148f89f59af11da1af668a9c839628ef
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-25 19:27:35 -0300

    Implementada estrutura básica do projeto

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        // Versão 1: Estrutura básica e entrada do usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("-------------------------------------");
+
+                Console.Write("Digite uma letra: "); 
+                char chute = Console.ReadLine()[0]; // obtém apenas um caractere do que o usuário digita
+
+                Console.WriteLine(chute);
+
+                Console.ReadLine();
+            }
         }
     }
 }
