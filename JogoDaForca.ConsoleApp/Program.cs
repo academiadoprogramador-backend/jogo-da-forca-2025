81c4632ab9e3e714116c66e26c59e8a0bae7b0f1
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-25 20:35:16 -0300

    Implementadas condições de vitória e derrota

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,12 +2,12 @@
 {
     internal class Program
     {
-        // Versão 2: Exibir palavra oculta com traços
+        // Versão 3: Verificação da entrada de dados do usuário 
         static void Main(string[] args)
         {
             while (true)
             {
-                string palavraSecreta = "MELANCIA";
+                string palavraSecreta = "UVA";
 
                 char[] letrasEncontradas = new char[palavraSecreta.Length];
 
@@ -17,19 +17,62 @@
                     letrasEncontradas[caractere] = '_';
                 }
 
-                string dicaDaPalavra = String.Join("", letrasEncontradas);
+                int quantidadeErros = 0;
+                bool jogadorEnforcou = false;
+                bool jogadorAcertou = false;
 
-                Console.Clear();
-                Console.WriteLine("-------------------------------------");
-                Console.WriteLine("Jogo da Forca");
-                Console.WriteLine("-------------------------------------");
-                Console.WriteLine("Palavra secreta: " + dicaDaPalavra);
-                Console.WriteLine("-------------------------------------");
+                do
+                {
+                    string dicaDaPalavra = String.Join("", letrasEncontradas);
 
-                Console.Write("Digite uma letra: "); 
-                char chute = Console.ReadLine()[0]; // obtém apenas um caractere do que o usuário digita
+                    Console.Clear();
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine("Jogo da Forca");
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine("Palavra secreta: " + dicaDaPalavra);
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine("Quantidade de erros: " + quantidadeErros);
+                    Console.WriteLine("-------------------------------------");
 
-                Console.WriteLine(chute);
+                    Console.Write("Digite uma letra: ");
+                    char chute = Console.ReadLine()[0];
+
+                    bool letraFoiEncontrada = false;
+
+                    for (int contador = 0; contador < palavraSecreta.Length; contador++)
+                    {
+                        char letraAtual = palavraSecreta[contador];
+
+                        if (chute == letraAtual)
+                        {
+                            letrasEncontradas[contador] = letraAtual;
+                            letraFoiEncontrada = true;
+                        }
+                    }
+
+                    if (letraFoiEncontrada == false)
+                        quantidadeErros++;
+
+                    dicaDaPalavra = String.Join("", letrasEncontradas);
+
+                    jogadorAcertou = dicaDaPalavra == palavraSecreta;
+                    // O jogador poderá cometer 5 erros antes de perder.
+                    jogadorEnforcou = quantidadeErros > 5;
+
+                    if (jogadorAcertou)
+                    {
+                        Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("Você acertou a palavra secreta! Era: " + palavraSecreta);
+                        Console.WriteLine("-------------------------------------");
+                    }
+                    else if (jogadorEnforcou)
+                    {
+                        Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("Que azar, tente novamente! A palavra era: " + palavraSecreta);
+                        Console.WriteLine("-------------------------------------");
+                    }
+
+                } while (jogadorAcertou == false && jogadorEnforcou == false); // || = OU
 
                 Console.ReadLine();
             }
